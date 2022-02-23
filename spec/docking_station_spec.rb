76fd8c3dd0125acd71c4e30606e4fbe893b1e4a0
jazzc001 <jazzcheung212@gmail.com>
2022-02-23 15:26:56 +0000

    Added gets a bike test to return an instance of a bike class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,6 +3,11 @@ require './lib/docking_station'
 
 describe DockingStation do
   it "returns release bike" do
-    expect(subject).to respond_to('release_bike')
+    expect(subject).to respond_to(:release_bike)
   end
+
+  it "get a bike" do
+    expect(subject.release_bike).to be_an_instance_of(Bike)
+  end
+
 end
