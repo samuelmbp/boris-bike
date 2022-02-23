d08a6147da17c04c6d82b40e97a338b7bde5c8e7
Samuel Raducan <samuel.thembp@gmail.com>
2022-02-23 14:44:23 +0000

    Add test for working? method and pass the test succesfully

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,3 +1,4 @@
 class Bike
-
+    def working?
+    end
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,5 +1,7 @@
 require './lib/bike'
 
 describe Bike do 
-    
+    it 'returns working?' do
+        expect(subject).to respond_to('working?')
+    end
 end
